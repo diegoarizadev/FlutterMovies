bbdf2182825add266d6c33365df7f36bb2e1e2d5
Diego Alejandro Ariza Medina <diego.ariza2@gmail.com>
2021-06-01 14:27:32 -0500

    Se realiza la implentacion del widget Swiper, Nota: Para ejecutar el proyecto, se debe realizar desde la consola con el siguiente comando : flutter run --no-sound-null-safety

--- movies/lib/src/pages/home_page.dart
+++ movies/lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatefulWidget {
   //HomePage({Key home}) : super(key: key);
@@ -11,7 +12,46 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Text('Movies')),
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('Peliculas'),
+        backgroundColor: Colors.orange,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.search,
+              color: Colors.black,
+            ),
+          )
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: [_swiperCards()],
+        ),
+      ),
+    );
+  }
+
+  _swiperCards() {
+    return Container(
+      padding: EdgeInsets.only(top: 10.0),
+      width: double.infinity, //Utiliza todo el ancho posible
+      height: 300.0,
+      child: Swiper(
+        layout: SwiperLayout.STACK,
+        itemWidth: 300.0,
+        itemBuilder: (BuildContext context, int index) {
+          return new Image.network(
+            "https://dummyimage.com/600x400/000/fff",
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: 3,
+        pagination: new SwiperPagination(),
+        control: new SwiperControl(),
+      ),
     );
   }
 }
