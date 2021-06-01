4a422c6b8ddaba5dd8ca235769a66616a3b0c3d1
Diego Alejandro Ariza Medina <diego.ariza2@gmail.com>
2021-06-01 16:26:21 -0500

    Se agrega el modelo y lógica necesaria ha  apartir de la respuesta del servicio

---
+++ movies/lib/src/models/movies_model.dart
@@ -0,0 +1,66 @@
+class Movies {
+  List<Film> items = new List<Film>.empty(growable: true);
+
+  Movies();
+
+  Movies.fromJsonList(List<dynamic> jsonList) {
+    // ignore: unnecessary_null_comparison
+    if (jsonList == null) return;
+
+    for (var item in jsonList) {
+      final film = new Film.fromJsonMap(item);
+      items.add(film);
+    }
+  }
+}
+
+class Film {
+  bool adult = false;
+  String backdropPath = '';
+  List<int> genreIds = [];
+  int id = 0;
+  String originalLanguage = '';
+  String originalTitle = '';
+  String overview = '';
+  double popularity = 0.0;
+  String posterPath = '';
+  String releaseDate = '';
+  String title = '';
+  bool video = false;
+  double voteAverage = 0.0;
+  int voteCount = 0;
+
+  Film({
+    required this.adult,
+    required this.backdropPath,
+    required this.genreIds,
+    required this.id,
+    required this.originalLanguage,
+    required this.originalTitle,
+    required this.overview,
+    required this.popularity,
+    required this.posterPath,
+    required this.releaseDate,
+    required this.title,
+    required this.video,
+    required this.voteAverage,
+    required this.voteCount,
+  });
+
+  Film.fromJsonMap(Map<String, dynamic> json) {
+    adult = json['adult'];
+    backdropPath = json['backdrop_path'];
+    genreIds = json['genre_ids'].cast<int>(); //Se convierte a entero
+    id = json['id'];
+    originalLanguage = json['original_language'];
+    originalTitle = json['original_title'];
+    overview = json['overview'];
+    popularity = json['popularity'] / 1; //Se convierte en double
+    posterPath = json['poster_path'];
+    releaseDate = json['release_date'];
+    title = json['title'];
+    video = json['video'];
+    voteAverage = json['vote_average'] / 1;
+    voteCount = json['vote_count'];
+  }
+}
