959eb805c5169cd83dede41bfe17944c27f6319c
Diego Alejandro Ariza Medina <diego.ariza2@gmail.com>
2021-06-03 12:35:41 -0500

    Se agrega la imlpementación del Search apuntando a los servicios del proveedor.

--- movies/lib/src/search/search_delegate.dart
+++ movies/lib/src/search/search_delegate.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:movies/src/models/movies_model.dart';
+import 'package:movies/src/providers/movies_provider.dart';
 
 class MovieSearch extends SearchDelegate {
   final peliculas = [
@@ -12,6 +14,8 @@ class MovieSearch extends SearchDelegate {
   final peliculasRecientes = ['spiderman', 'cruella'];
 
   String selection = '';
+
+  final moviesProvider = MoviesProvider();
 
   @override
   List<Widget> buildActions(BuildContext context) {
@@ -41,14 +45,7 @@ class MovieSearch extends SearchDelegate {
   @override
   Widget buildResults(BuildContext context) {
     // La instruccion que crea los resultados que se van a buscar
-    return Center(
-      child: Container(
-        height: 100.0,
-        width: 100.0,
-        color: Colors.blue,
-        child: Text(selection),
-      ),
-    );
+    return Container();
   }
 
   @override
@@ -57,23 +54,44 @@ class MovieSearch extends SearchDelegate {
 
     //aplicar un filtro a un listado y la propiedad query estara mutando o cambiando.
 
-    final listaSugerida = (query.isEmpty)
-        ? peliculasRecientes
-        : peliculas
-            .where((p) => p.toLowerCase().startsWith(query.toLowerCase()))
-            .toList(); //Retonar todos los resultados segun la busqueda.
+    if (query.isEmpty) {
+      //Validación para cuando los query esten vacios.
+      return Container();
+    } else {
+      return FutureBuilder(
+        future: moviesProvider.getSearchMovie(query),
+        builder: (BuildContext context, AsyncSnapshot<List<Film>> snapshot) {
+          if (snapshot.hasData) {
+            //Tiene Data?
 
-    return ListView.builder(
-        itemCount: listaSugerida.length,
-        itemBuilder: (context, i) {
-          return ListTile(
-            leading: Icon(Icons.movie),
-            title: Text(listaSugerida[i]),
-            onTap: () {
-              selection = listaSugerida[i];
-              showResults(context); //Muestra los resultados
-            },
-          );
-        });
+            final movies = snapshot.data;
+
+            return ListView(
+                children: movies!.map((movie) {
+              return ListTile(
+                leading: FadeInImage(
+                  image: NetworkImage(movie.getPosterImage()),
+                  placeholder: AssetImage('assets/img/no-image.jpg'),
+                  width: 50.0,
+                  fit: BoxFit.contain,
+                ),
+                title: Text(movie.title),
+                subtitle: Text(movie.originalTitle),
+                onTap: () {
+                  close(context, null); //Cerrar la busqueda
+                  movie.uniqueID = '';
+                  //Navegar al detalle
+                  Navigator.pushNamed(context, 'details', arguments: movie);
+                },
+              );
+            }).toList());
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      );
+    }
   }
 }
