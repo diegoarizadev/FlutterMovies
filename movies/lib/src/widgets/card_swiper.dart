914d589d19d9cb75ac8ad54109a98ff7aba7e999
Diego Alejandro Ariza Medina <diego.ariza2@gmail.com>
2021-06-22 22:31:17 -0500

    flutter_swiper: ^1.1.6 a carousel_slider: ^4.0.0

--- movies/lib/src/widgets/card_swiper.dart
+++ movies/lib/src/widgets/card_swiper.dart
@@ -1,5 +1,5 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:movies/src/models/movies_model.dart';
 
 class CardSwiperCustom extends StatelessWidget {
@@ -14,40 +14,63 @@ class CardSwiperCustom extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //Ajuste de pantallas en los diferentes dispositivos.
-    final _screenSize = MediaQuery.of(context).size;
+    //final _screenSize = MediaQuery.of(context).size;
+    return CarouselSlider.builder(
+        itemCount: this.movies.length,
+        itemBuilder: (context, index, realIndex) => PosterImage(
+              movie: this.movies[index],
+            ),
+        options: CarouselOptions(
+          autoPlay: true,
+          aspectRatio: 2.0,
+          enlargeCenterPage: true,
+        ));
+    // return Container(
+    //   padding: EdgeInsets.only(top: 30.0),
+    //   child: Swiper(
+    //     itemWidth: _screenSize.width * 0.7, //70%
+    //     itemHeight: _screenSize.height * 0.5, //50%
+    //     layout: SwiperLayout.STACK,
+    //     itemBuilder: (BuildContext context, int index) {
+    //       //construye la tarjeta
 
-    return Container(
-      padding: EdgeInsets.only(top: 30.0),
-      child: Swiper(
-        itemWidth: _screenSize.width * 0.7, //70%
-        itemHeight: _screenSize.height * 0.5, //50%
-        layout: SwiperLayout.STACK,
-        itemBuilder: (BuildContext context, int index) {
-          //construye la tarjeta
+    //       movies[index].uniqueID =
+    //           '${movies[index].id}-card'; //Se instancia la propiedad del modelo para generar un ID unico.
 
-          movies[index].uniqueID =
-              '${movies[index].id}-card'; //Se instancia la propiedad del modelo para generar un ID unico.
+    //       return Hero(
+    //         tag: movies[index].uniqueID,
+    //         child: ClipRRect(
+    //             borderRadius: BorderRadius.circular(20.0),
+    //             child: GestureDetector(
+    //               //Implementación del GestureDector en el card de las peliculas mas populares
+    //               onTap: () => Navigator.pushNamed(context, 'details',
+    //                   arguments: movies[index]),
+    //               child: posterImage(movies: movies),
+    //             )),
+    //       );
+    //     },
+    //     itemCount: movies.length,
+    //     //pagination: new SwiperPagination(),
+    //     //control: new SwiperControl(),
+    //   ),
+    // );
+  }
+}
 
-          return Hero(
-            tag: movies[index].uniqueID,
-            child: ClipRRect(
-                borderRadius: BorderRadius.circular(20.0),
-                child: GestureDetector(
-                  //Implementación del GestureDector en el card de las peliculas mas populares
-                  onTap: () => Navigator.pushNamed(context, 'details',
-                      arguments: movies[index]),
-                  child: FadeInImage(
-                    image: NetworkImage(movies[index].getPosterImage()),
-                    placeholder: AssetImage('assets/img/no-image.jpg'),
-                    fit: BoxFit.cover,
-                  ),
-                )),
-          );
-        },
-        itemCount: movies.length,
-        //pagination: new SwiperPagination(),
-        //control: new SwiperControl(),
-      ),
+class PosterImage extends StatelessWidget {
+  const PosterImage({
+    Key? key,
+    required this.movie,
+  }) : super(key: key);
+
+  final Film movie;
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeInImage(
+      image: NetworkImage(movie.getPosterImage()),
+      placeholder: AssetImage('assets/img/no-image.jpg'),
+      fit: BoxFit.cover,
     );
   }
 }
